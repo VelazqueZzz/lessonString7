3502fde51cc97514adce4f1bab17c0fbe8cf7db2
VelazqueZzz <m.italy@vk.com>
2026-09-25 12:23:30 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+        var from = "E2"
+        var to = "E4"
+        var moveNumber = 1
+
+        val move1 = "[$from-$to;$moveNumber]"
+        println(move1)
+
+        from = "D2"
+        to = "D3"
+        moveNumber = 2
+
+        val move2 = "[$from-$to;$moveNumber]"
+        println(move2)
+    }
